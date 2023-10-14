2a476c076a8fcb20a279dcc47bc02010ed9f7aad
SanthoshRamana <53848645+santhosh-patchigolla@users.noreply.github.com>
2023-10-14 18:42:17 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "abhi/terraform.tfstate"  #path in S3 bucket to store statefile
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-lock" # Created a resouse(FYI main.tf); This is used for locking purpose where to avoid multiples people changes at a time, once the locking is done only one person can excute the changes and the other need to wait until its unlock(completion of the appply command) 
   }
 }
